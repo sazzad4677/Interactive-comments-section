3c648b89ab93be9509d1e736941b1d4ab090b802
sazzad4677 <sazzad35-1898@diu.edu.bd>
2022-03-05 21:00:27 +0600

    color defined and test

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// opacity value will be 1 if no opacity defined
+function opacityCalculation(variable) {
+  return ({ opacityValue }) => `hsla(var(${variable}), ${opacityValue || 1} )`;
+}
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        primary: {
+          "moderate-blue": opacityCalculation("--primary-moderate-blue"),
+          "soft-red": opacityCalculation("--primary-soft-red"),
+          "pale-red": opacityCalculation("--primary-pale-red"),
+        },
+        neutral: {
+          "dark-blue": opacityCalculation("--neutral-dark-blue"),
+          "grayish-blue": opacityCalculation("--neutral-grayish-blue"),
+          "light-gray": opacityCalculation("--neutral-light-gray"),
+          "very-light-gray": opacityCalculation("--neutral-very-light-gray"),
+          white: opacityCalculation("--neutral-white"),
+        },
+      },
+    },
+  },
+  plugins: [],
+};
